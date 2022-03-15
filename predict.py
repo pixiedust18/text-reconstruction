ac271be92fa0224808d8ab52c77edbc5f37e0ac9
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2022-03-16 05:22:49 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,11 @@
+"""
+SRNet - Editing Text in the Wild
+Data prediction.
+Copyright (c) 2019 Netease Youdao Information Technology Co.,Ltd.
+Licensed under the GPL License (see LICENSE for details)
+Written by Krisha Bhambani
+"""
+
 from model import SRNet
 import numpy as np
 import os
@@ -7,27 +15,29 @@ from datagen import srnet_datagen, get_input_data
 import argparse
 from craft_text_detector import Craft
 from google.colab.patches import cv2_imshow
+import matplotlib.pyplot as plt 
+
 from reconstruction_utils import *
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
-
+            
+print_log('model compiling start.', content_color = PrintColor['yellow'])
+model = SRNet(shape = cfg.data_shape, name = 'predict')
+print_log('model compiled.', content_color = PrintColor['yellow'])
+load_wt_flag = False
 
 def SRNet_execute(i_s_path, i_t_path, checkpoint, save_dir, input_dir=None):
-    # define model
-    print_log('model compiling start.', content_color = PrintColor['yellow'])
-    model = SRNet(shape = cfg.data_shape, name = 'predict')
-    print_log('model compiled.', content_color = PrintColor['yellow'])
-
     with model.graph.as_default():
         with tf.Session() as sess:
-            saver = tf.train.Saver(tf.global_variables())
-            
             # load pretrained weights
+            saver = tf.train.Saver(tf.global_variables())
+            #if not load_wt_flag:
             print_log('weight loading start.', content_color = PrintColor['yellow'])
             saver.restore(sess, checkpoint)
             print_log('weight loaded.', content_color = PrintColor['yellow'])
-            
+            load_wt_flag = True
+
             # predict
             print_log('predicting start.', content_color = PrintColor['yellow'])
             if input_dir is None:
@@ -69,7 +79,7 @@ def main():
 
     assert args.save_dir is not None
     assert args.checkpoint is not None
-
+    
     # gpu
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
     if not os.path.exists('results/'):
@@ -80,10 +90,14 @@ def main():
       os.mkdir('cur_test/')
     if not os.path.exists('craft_outputs/'):
       os.mkdir('craft_outputs/')
-      
+
+     
     img_path = args.img_dir
     img_name = args.img_name
     img= cv2.imread(img_path)
+    #orig = cv2.imread(img_path)
+    #orig_copy = cv2.imread(img_path)
+
     print(img_name)
     if abs(img.shape[0] - img.shape[1])>150:
       if (img.shape[0]<img.shape[1]):
@@ -127,11 +141,36 @@ def main():
       flag = True
     word_list = my_autocorrect(words, incomplete_word, flag, start, end)
     letter_write = missing_letters(word_list['Word'].iloc[0].lower(), incomplete_word)
-    non_stylised_gen(img_name, img_path, letter_write)
-    SRNet_execute('cur_test/i_s.png', 'cur_test/i_t.png', args.checkpoint, save_dir='results/') ####
-    result_path = 'results/result.png'
-    res = final_integration(img, img_name, letter_write, word_list)
-    cv2_imshow(res)
+    col = np.mean(img)
+    pref_col = 0
+    if col<150:
+        pref_col = 1
+    elif col<180:
+        pref_col = 2
+    else:
+        pref_col = 3
+
+    final_res = np.array([])
+    for i in range(4):
+        non_stylised_gen(img_name, img_path, letter_write, i)
+        SRNet_execute('cur_test/i_s.png', 'cur_test/i_t.png', args.checkpoint, save_dir='results/') ####
+        result_path = 'results/result.png'
+        pred_letter, res = final_integration(img_name,img_path, letter_write, word_list, i)
+        #plt.imshow(res)
+        cv2.imwrite(str(i)+'.png', res)
+
+        print(pred_letter)
+        x = letter_write.pop()
+        letter_write.add(x)
+        if not final_res.any() and pref_col == i:
+            final_res = res
+        if i==3 and not final_res.any():
+            final_res = res
+        if pred_letter.lower()==x.lower():
+            final_res = res
+            break
+        
+    cv2_imshow(final_res)
     cv2.imwrite('results/reconstructed_text.png', res)
     
                 
